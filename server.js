fd542b62b0e14f79ded3e1876e87f1a950e4ac64
DocJek <bassroscoe@gmail.com>
2017-07-19 11:20:27 -0700

    fixed bug with site not loading on localhost

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
-app.use(express.static('./portfolio'));
+app.use(express.static('.'));
 
 app.listen(PORT, function() {
   console.log('Listening on port: ' + PORT);
