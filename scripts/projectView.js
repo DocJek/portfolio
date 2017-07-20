c2a500ae8455ea05d994941f7a65d92919467413
DocJek <bassroscoe@gmail.com>
2017-07-20 09:59:44 -0700

    added styling to imgs borders and .thumb for centering on page

--- scripts/projectView.js
+++ scripts/projectView.js
@@ -4,8 +4,17 @@ var projectView = {};
 
 projectView.handleNavBar = function() {
   $('.navbar').on('click', '.tab', function() {
-    $('.tab-content').hide();
+    $('.tabContent').hide();
     $('#' + $(this).data('content')).fadeIn();
   });
   $('.navbar .tab:first').click()
 }
+
+//
+//   $('.main-nav').on('click', '.tab', function() {
+//     $('.tab-content').hide();                     // hide everything on the click event
+//     $('#' + $(this).data('content')).show();       // then show just what was wanted
+//   });
+//
+//   $('.main-nav .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
+// };
