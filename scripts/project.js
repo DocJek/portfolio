d0145cc7c6a349ea7cc2ad9273a30173fe327e8f
DocJek <bassroscoe@gmail.com>
2017-07-24 11:39:35 -0700

    Added IIFE to project.js, verified app still working

--- scripts/project.js
+++ scripts/project.js
@@ -1,26 +1,29 @@
 'use strict';
 
-var projects = [];
+(function (module) {
+  var projects = [];
 
-function Project(projectData) {
-  this.title = projectData.title;
-  this.img = 'images/' + projectData.img,
-  this.link = projectData.link;
-  this.caption = projectData.caption;
-}
+  function Project(projectData) {
+    this.title = projectData.title;
+    this.img = 'images/' + projectData.img,
+    this.link = projectData.link;
+    this.caption = projectData.caption;
+  }
 
-// responsible for transferring project data to html via Handlebars.
-Project.prototype.toHtml = function() {
-  var $newProject = $('#handlebarsTemplate').html();
-  var compiled = Handlebars.compile($newProject);
-  var html = compiled(this);
-  $('#projects').append(html);
-};
+  // responsible for transferring project data to html via Handlebars.
+  Project.prototype.toHtml = function() {
+    var $newProject = $('#handlebarsTemplate').html();
+    var compiled = Handlebars.compile($newProject);
+    var html = compiled(this);
+    $('#projects').append(html);
+  };
 
-projectData.forEach(function(projectObj) {
-  projects.push(new Project(projectObj));
-});
+  projectData.forEach(function(projectObj) {
+    projects.push(new Project(projectObj));
+  });
 
-projects.forEach(function(project) {
-  $('#projects').append(project.toHtml());
-});
+  projects.forEach(function(project) {
+    $('#projects').append(project.toHtml());
+  });
+  module.Project = Project;
+})();
