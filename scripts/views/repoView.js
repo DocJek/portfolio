750f273435480f17c6f8a1ec9c2e303f5dfc4aa8
DocJek <bassroscoe@gmail.com>
2017-07-29 15:24:10 -0700

    reverted to when request to github still worked

--- scripts/views/repoView.js
+++ scripts/views/repoView.js
@@ -4,18 +4,18 @@ var app = app || {};
 (function(module) {
   const repoView = {};
   const ui = function() {
+
+
     $('.tabContent').hide();
     $('#about').fadeIn(1000);
   };
 
-  module.displayRepos = function () {
-    let render = Handlebars.compile($('#repo-template').html());
-    repoView.index = function() {
-      ui();
+  let render = Handlebars.compile($('#repo-template').html());
+  repoView.index = function() {
+    ui();
 
-      $('#about ul').append(app.repos.all.map(render));
-    };
-  }
+    $('#about ul').append(app.repos.all.map(render));
+  };
 
   module.repoView = repoView;
 })(app);
